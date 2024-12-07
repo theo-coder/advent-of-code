44a7c8fbf4a80b1be4e180df5bf611d19dd4d2ca
Théo Posty <theo@posty.fr>
2024-12-07 15:19:48 +0100

    feat: day-07 - p2

--- 2024/day-07/src/bin/part2.rs
+++ 2024/day-07/src/bin/part2.rs
@@ -5,19 +5,83 @@ fn main() {
 }
 
 fn solve_part2(input: &str) -> i64 {
-    todo!()
+    let mut result = 0;
+
+    let operations: Vec<_> = input
+        .lines()
+        .map(|l| {
+            let (res, nbs) = l.split_once(": ").unwrap();
+            (
+                res.parse::<i64>().unwrap(),
+                nbs.split(" ")
+                    .map(|nb| nb.parse::<i64>().unwrap())
+                    .collect::<Vec<_>>(),
+            )
+        })
+        .collect();
+
+    'outer: for operation in &operations {
+        let obj = operation.0;
+
+        let mut possible_operations = Vec::new();
+        let nb_calc = 3_i64.pow((operation.1.len() - 1) as u32);
+
+        for i in 0..nb_calc {
+            let mut calc_tpl = String::new();
+            let mut nb = i;
+
+            while calc_tpl.len() < (operation.1.len() - 1) {
+                calc_tpl.insert(0, char::from_digit((nb % 3) as u32, 3).unwrap());
+                nb /= 3;
+            }
+            possible_operations.push(calc_tpl);
+        }
+
+        for op in possible_operations {
+            let res = calc_from_binary(&operation.1, &op);
+            if res == obj {
+                result += res;
+                continue 'outer;
+            }
+        }
+    }
+
+    result
+}
+
+fn calc_from_binary(nbs: &[i64], binary_ops: &str) -> i64 {
+    let mut result = nbs[0];
+
+    for (nb_idx, nb) in nbs.iter().enumerate().skip(1) {
+        match binary_ops.chars().nth(nb_idx - 1).unwrap() {
+            '0' => result += nb,
+            '1' => result *= nb,
+            '2' => result = format!("{}{}", result, nb).parse::<i64>().unwrap(),
+            _ => unreachable!(),
+        }
+    }
+
+    result
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT: &str = "";
+    const INPUT: &str = "190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20";
 
     #[test]
     fn it_works() {
         let result = solve_part2(INPUT);
 
-        assert_eq!(result, 0);
+        assert_eq!(result, 11387);
     }
 }
