068cf3ebaf447e4b311b7a8cad10e2740dfe1c72
Théo Posty <theo@posty.fr>
2024-12-10 17:45:24 +0100

    feat: day-10

---
+++ 2024/day-10/src/bin/part1.rs
@@ -0,0 +1,91 @@
+use std::collections::{HashMap, HashSet};
+
+fn main() {
+    let input = include_str!("../input.txt");
+    let output = solve_part1(input);
+    dbg!(output);
+}
+
+fn solve_part1(input: &str) -> usize {
+    let mut map = Vec::new();
+    let mut potential_heads = Vec::new();
+
+    for (l_idx, line) in input.lines().enumerate() {
+        map.push(Vec::new());
+        for (c_idx, nb) in line.chars().map(|c| c.to_digit(10).unwrap()).enumerate() {
+            map[l_idx].push(nb);
+            if nb == 0 {
+                potential_heads.push(((c_idx, l_idx), (c_idx, l_idx), 1));
+            }
+        }
+    }
+
+    let mut trails_from_head = HashMap::new();
+
+    potential_heads.iter().for_each(|h| {
+        trails_from_head.insert(h.0, 0);
+    });
+
+    let mut done_tail_with_head = HashSet::new();
+
+    while let Some((current, initial, level)) = potential_heads.pop() {
+        if level > 9 {
+            if !done_tail_with_head.contains(&(initial, current)) {
+                done_tail_with_head.insert((initial, current));
+                *trails_from_head.get_mut(&initial).unwrap() += 1;
+            }
+            continue;
+        }
+
+        let possible_bounds = check_bounds(current, level, &map);
+
+        for possible_bound in possible_bounds {
+            potential_heads.push((possible_bound, initial, level + 1));
+        }
+    }
+
+    trails_from_head.iter().map(|t| t.1).sum()
+}
+
+fn check_bounds(head: (usize, usize), i: usize, map: &[Vec<u32>]) -> Vec<(usize, usize)> {
+    let mut possible_bounds = Vec::new();
+
+    if head.1 > 0 && map[head.1 - 1][head.0] == i as u32 {
+        possible_bounds.push((head.0, head.1 - 1));
+    }
+
+    if head.1 < map.len() - 1 && map[head.1 + 1][head.0] == i as u32 {
+        possible_bounds.push((head.0, head.1 + 1));
+    }
+
+    if head.0 > 0 && map[head.1][head.0 - 1] == i as u32 {
+        possible_bounds.push((head.0 - 1, head.1));
+    }
+
+    if head.0 < map[0].len() - 1 && map[head.1][head.0 + 1] == i as u32 {
+        possible_bounds.push((head.0 + 1, head.1));
+    }
+
+    possible_bounds
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732";
+
+    #[test]
+    fn it_works() {
+        let result = solve_part1(INPUT);
+
+        assert_eq!(result, 36);
+    }
+}
